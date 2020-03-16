6c6edd92e9f0ffdf940d27f6f6e91040425edaeb
Uladzimir Makarau <gasmakarov.gmail.com>
2020-03-17 01:13:53 +0300

    Add Element Interaction, Part 2

--- test/element.test.js
+++ test/element.test.js
@@ -1,7 +1,10 @@
+const internetPage = require("../page/internet.page");
 describe("Interacting with elements", function() {
   it("Get text for elements", function() {
     browser.url("/");
     let text = $("#page-footer").getText();
     console.log(text);
+    internetPage.getLiText();
+    internetPage.getSpecificElementText(3);
   });
 });
