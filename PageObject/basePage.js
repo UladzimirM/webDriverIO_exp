31ff0e70177c7e58f9d5ba186d4de45a4cd8ef00
Uladzimir Makarau <gasmakarov.gmail.com>
2020-03-21 22:37:57 +0300

    Elements Action Using Keyboard and Mouse Input

--- PageObject/basePage.js
+++ PageObject/basePage.js
@@ -20,11 +20,46 @@ class BasePage {
   get firstLink() {
     return $('ul li:nth-child(1) a')
   }
+
   get username() {
     return $('#username')
   }
   get password() {
     return $('#password')
+  }
+  get targetValue() {
+    return $('.example #target')
+  }
+  get resultValue() {
+    return $('.example #result')
+  }
+
+  clickTargetValue() {
+    this.targetValue.waitForDisplayed()
+    this.targetValue.click()
+  }
+  sendToTarget(text) {
+    this.targetValue.waitForDisplayed()
+    this.targetValue.keys(text)
+  }
+  getResultText() {
+    this.resultValue.waitForDisplayed()
+    return this.resultValue.getText()
+  }
+  figures(index) {
+    return $(`.example .figure:nth-child(${index}) img`)
+  }
+  figureDetails(index) {
+    return $(`.example .figure:nth-child(${index}) .figcaption h5`)
+  }
+
+  hoverOnFigure(index) {
+    this.figures(index).waitForDisplayed()
+    this.figures(index).moveTo(1, 1)
+  }
+  getFigureDetailsText(index) {
+    this.figureDetails(index).waitForDisplayed()
+    return this.figureDetails(index).getText()
   }
   link(index) {
     return $(`ul li:nth-child(${index}) a`)
@@ -32,12 +67,13 @@ class BasePage {
   checkboxes(index) {
     return $(`#checkboxes input:nth-child(${index})`)
   }
-  specificChildElement(index) {
-    return this.parent.$(`li:nth-child(${index})`)
-  }
+
   clickCheckBox(index) {
     this.checkboxes(index).waitForDisplayed()
     this.checkboxes(index).click()
+  }
+  specificChildElement(index) {
+    return this.parent.$(`li:nth-child(${index})`)
   }
   getLiText() {
     this.childElements.filter(element => {
@@ -50,7 +86,7 @@ class BasePage {
     return this.specificChildElement(index).getText()
   }
   clickLink(index) {
-    this.link(index).isDisplayed
+    this.link(index).isDisplayed()
     this.link(index).click()
   }
   clickOnLink() {
---
+++ test/webdriverioAction.test.js
@@ -0,0 +1,37 @@
+const basePage = require('../PageObject/basePage')
+describe('webdriverIO API Actions', function() {
+  it('should hover on figure 1', function() {
+    browser.url(`${browser.options.baseUrl}hovers`)
+    basePage.hoverOnFigure(3)
+    assert.equal('name: user1', basePage.getFigureDetailsText(3))
+  })
+  it('should hover on figure 2', function() {
+    browser.url(`${browser.options.baseUrl}hovers`)
+    basePage.hoverOnFigure(4)
+    assert.equal('name: user2', basePage.getFigureDetailsText(4))
+  })
+  it('should hover on figure 3', function() {
+    browser.url(`${browser.options.baseUrl}hovers`)
+    basePage.hoverOnFigure(5)
+    assert.equal('name: user3', basePage.getFigureDetailsText(5))
+  })
+  it('should send keyboard value Backspace', function() {
+    browser.url(`${browser.options.baseUrl}/key_presses`)
+    basePage.clickTargetValue()
+    basePage.sendToTarget('\uE003')
+    assert.equal('You entered: BACK_SPACE', basePage.getResultText())
+  })
+  it('should send keyboard value Tab', function() {
+    browser.url(`${browser.options.baseUrl}/key_presses`)
+    basePage.clickTargetValue()
+    basePage.sendToTarget('\uE004')
+    assert.equal('You entered: TAB', basePage.getResultText())
+  })
+  // prettier-ignore
+  it('should send keyboard value Shift', function() {
+    browser.url(`${browser.options.baseUrl}/key_presses`)
+    basePage.clickTargetValue()
+    basePage.sendToTarget("Shift")
+    assert.equal('You entered: SHIFT', basePage.getResultText())
+  })
+})
